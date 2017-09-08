1755241b04bc2e9ad1b7ef50d8faae2e0f0c7958
Unknown <riderjensen1@gmail.com>
2017-09-07 21:23:25 -0600

    Added accordion
    Need to fix elements not working on load and only working after a while loading

--- Project One Starter File/js/main.js
+++ Project One Starter File/js/main.js
@@ -1,1 +1,12 @@
 
+
+$('#myAffix').affix({
+  offset: {
+    top: 100,
+    bottom: function () {
+      return (this.bottom = $('.footer').outerHeight(true));
+    }
+  }
+});
+
+$('.collapse').collapse();
