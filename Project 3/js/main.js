e03203b2054898160cfc90f433ca4a7375adf653
Unknown <riderjensen1@gmail.com>
2017-10-12 21:46:40 -0600

    Finished Project 3

--- Project 3/js/main.js
+++ Project 3/js/main.js
@@ -2,8 +2,11 @@
 TweenMax.to(".ul1", 3, {top: 0});
 TweenMax.to(".logo", 2, {left: 25});
 TweenMax.to(".banner-image", 1, {opacity: 1, delay: 2});
-TweenMax.to(".far-right-thumb", 1, {opacity: 1, delay: 3});
-TweenMax.to(".pic2", 1, {opacity: 1, delay: 3});
-TweenMax.to(".pic3", 1, {opacity: 1, delay: 3});
-TweenMax.to(".pic4", 1, {opacity: 1, delay: 3});
 TweenMax.to(".myBackgroundImage", 1, {opacity: 1, delay: 3});
+
+function showThumbnails(){
+	TweenMax.to(".far-right-thumb", 1, {opacity: 1, delay: 1});
+	TweenMax.to(".pic2", 1, {opacity: 1, delay: 1});
+	TweenMax.to(".pic3", 1, {opacity: 1, delay: 1});
+	TweenMax.to(".pic4", 1, {opacity: 1, delay: 1});
+}
