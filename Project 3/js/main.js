acaefa7054872d968b70f9e2e6c3258326550ab2
Unknown <riderjensen1@gmail.com>
2017-10-12 19:15:39 -0600

    Animated Logo, Menu, Banner, and Thumbnails

---
+++ Project 3/js/main.js
@@ -0,0 +1,9 @@
+
+TweenMax.to(".ul1", 3, {top: 0});
+TweenMax.to(".logo", 2, {left: 25});
+TweenMax.to(".banner-image", 1, {opacity: 1, delay: 2});
+TweenMax.to(".far-right-thumb", 1, {opacity: 1, delay: 3});
+TweenMax.to(".pic2", 1, {opacity: 1, delay: 3});
+TweenMax.to(".pic3", 1, {opacity: 1, delay: 3});
+TweenMax.to(".pic4", 1, {opacity: 1, delay: 3});
+TweenMax.to(".myBackgroundImage", 1, {opacity: 1, delay: 3});
